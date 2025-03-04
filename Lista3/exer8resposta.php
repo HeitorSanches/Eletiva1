fef2a9f7953bcef126660fd78a5cc25e7e886dcd
Heitor Sanches <147269105+HeitorSanches@users.noreply.github.com>
2025-03-04 14:25:36 -0300

    exercicio 8

---
+++ Lista3/exer8resposta.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body class="d-flex justify-content-center align-items-center vh-100">
+
+  <div class="text-center border p-4 rounded shadow bg-light">
+    <h1>Resposta do exercício 8</h1>
+
+    <?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    try {
+
+    
+        $valor = $_POST['valor'];
+        
+        echo "Contagem Regressiva : ";
+  
+        do
+        {
+            echo "$valor  ";
+            $valor = $valor -1;
+
+        }  while($valor >= 0);
+
+
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+}
+?>
+
+  </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
