ca1766149bdc58bf61169e1f89e9633af540cf9f
Heitor Sanches <147269105+HeitorSanches@users.noreply.github.com>
2025-03-04 15:52:08 -0300

    exercicio 12

--- Lista2/exer11resposta.php
+++ Lista2/exer11resposta.php
@@ -16,7 +16,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Resposta do exercício 9 </h1>
+    <h1>Resposta do exercício 11 </h1>
 
     <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
--- Lista2/exer12resposta.php
+++ Lista2/exer12resposta.php
@@ -6,32 +6,27 @@
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
-  <body>
-  <!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
-  <body>
-    <h1>Resposta do exercício 10 </h1>
+  <body class="d-flex justify-content-center align-items-center vh-100">
+  <div class="text-center border p-4 rounded shadow bg-light">
+    <h1>Resposta do exercício 12 </h1>
 
     <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     try {
-        $altura = $_POST['altura'];
-        $largura = $_POST['largura'];
+        $base = $_POST['base'];
+        $expoente = $_POST['exp'];
+        $potencia = $base ** $expoente;
 
-        $perimetro = ($altura * 2) +  ($largura * 2);
-        echo $perimetro;
+        echo "Sua potência resulta em $potencia ";
+
+
 
     } catch (Exception $e) {
         echo $e->getMessage();
     }
 }
 ?>
+  </div>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
