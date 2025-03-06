3a6edbfa0ad08e438e90edc6b740af70e4f1186c
Heitor Sanches <147269105+HeitorSanches@users.noreply.github.com>
2025-03-06 20:27:39 -0300

    Finalizado exercicio 1

--- Lista4/exer1.php
+++ Lista4/exer1.php
@@ -12,45 +12,10 @@
   <form method="post" action="exer1resposta.php">
                         
                         <div class="mb-3">
-                            <label for="valor1" class="form-label">Informe o primeiro valor :</label>
-                            <input type="number" id="valor1" name="valor1" class="form-control" required="">
+                            <label for="palavra" class="form-label">Digite uma palavra:</label>
+                            <input type="palavra" id="palavra" name="palavra" class="form-control" required="">
                         </div>
                     
-                        <div class="mb-3">
-                            <label for="valor2" class="form-label">Informe o segundo valor:</label>
-                            <input type="number" id="valor2" name="valor2" class="form-control" required="">
-                        </div>
-
-                                                
-                        <div class="mb-3">
-                            <label for="valor3" class="form-label">Informe o primeiro valor :</label>
-                            <input type="number" id="valor3" name="valor3" class="form-control" required="">
-                        </div>
-                    
-                        <div class="mb-3">
-                            <label for="valor4" class="form-label">Informe o segundo valor:</label>
-                            <input type="number" id="valor4" name="valor4" class="form-control" required="">
-                        </div>
-                    
-
-                                                
-                        <div class="mb-3">
-                            <label for="valor5" class="form-label">Informe o primeiro valor :</label>
-                            <input type="number" id="valor5" name="valor5" class="form-control" required="">
-                        </div>
-                    
-                        <div class="mb-3">
-                            <label for="valor6" class="form-label">Informe o segundo valor:</label>
-                            <input type="number" id="valor6" name="valor6" class="form-control" required="">
-                        </div>
-                    
-
-                                                
-                        <div class="mb-3">
-                            <label for="valor7" class="form-label">Informe o primeiro valor :</label>
-                            <input type="number" id="valor7" name="valor7" class="form-control" required="">
-                        </div>
-
                     
                     <button type="submit" class="btn btn-primary">Enviar</button>
     </form>
