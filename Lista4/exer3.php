283339ef475b8fa0f2ef0515622c808b1bb87ca1
Heitor Sanches <147269105+HeitorSanches@users.noreply.github.com>
2025-03-06 21:47:01 -0300

    Exercicio 3

---
+++ Lista4/exer3.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+
+
+  <form method="post" action="exer3resposta.php">
+                        
+                        <div class="mb-3">
+                            <label for="palavra1" class="form-label">Digite a primeira  palavra:</label>
+                            <input type="text" id="palavra" name="palavra1" class="form-control" required="">
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="palavra2" class="form-label">Digite a segunda palavra:</label>
+                            <input type="text" id="palavra" name="palavra2" class="form-control" required="">
+                        </div>
+                    
+                    
+                    <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ Lista4/exer3resposta.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+?>
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bootstrap demo</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+  <!doctype html>
+  <html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+
+  <body>
+    <h1>Resposta do exercício 3</h1>
+
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        function estaContido(string $palavra1, string $palavra2): void
+        {
+            $pos = strpos($palavra1,$palavra2);
+            if($pos == false)
+            {
+                echo "A segunda palavra não está contida na primeira";
+            }
+            else
+            {
+                echo "A segunda está contida na primeira na posicao $pos" ;
+            }
+        }
+
+      }
+
+      try {
+
+        $palavra1 = $_POST['palavra1'];
+        $palavra2 = $_POST['palavra2'];
+        estaContido($palavra1,  $palavra2);
+
+
+      } catch (Exception $e) {
+        echo $e->getMessage();
+      }
+    
+    ?>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+
+  </html>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
